23313e89ff733e478276f72f02578c1b582699f0
徐大大 <email@xluos.com>
2018-04-22 13:37:12 +0800

    所有功能都已实现

--- VueTodos/index.js
+++ VueTodos/index.js
@@ -1,3 +1,5 @@
+
+// 子组件
 var item = {
     props: ['itemdata'],
     template: 
@@ -7,22 +9,32 @@ var item = {
             +        '<label v-bind:for="\'item-\' + itemdata.id" v-bind:class="{ complete: itemdata.ok, nocomplete: !itemdata.ok}" @click="reOk()"></label>'
             +    '</div>'
             +    '<div class="todos-item-text">'
-            +        '<div class="text" v-show="!itemdata.edit" v-bind:class="{ strickout: itemdata.ok}">{{itemdata.text}}</div>'
-            +        '<input type="text" v-show="itemdata.edit" v-model="itemdata.text">'
+            +        '<div class="text" @dblclick="itemdata.edit=!itemdata.edit" v-show="!itemdata.edit" v-bind:class="{ strickout: itemdata.ok}">{{itemdata.text}}</div>'
+            +        '<input type="text" @blur="itemBlur" v-show="itemdata.edit" v-model.trim="itemdata.text">'
             +    '</div>'
             +    '<button class="shut" @click="remove">X</button>'
             +'</li>',
     methods: {
+        // 设置为已完成
         reOk: function() {
             this.itemdata.ok = !this.itemdata.ok;
+            this.$emit("updatasize",this.itemdata.ok?1:-1);
         },
+        // 和父组件通信删除这个选项
         remove: function(){
-            console.log(this);
-            this.$emit("reitems",this.itemdata.id);
+            this.$emit("reitems",this.itemdata);
+        },
+        // 编辑框失去焦点后如果内容为空则删除
+        itemBlur: function() {
+            if(this.itemdata.text.length) {
+                this.itemdata.edit=!this.itemdata.edit
+            } else {
+                this.remove();
+            }
         }
     }
 }
-          
+
 var app = new Vue({
     // 挂载对象
     el: '#app',
@@ -30,45 +42,77 @@ var app = new Vue({
     data: {
         message: '',
         todos: [
-            {
-                id: 0,
-                ok: false,
-                edit: false,
-                text: "123456",
-            },
+            // {
+            //     id: 0,          //id
+            //     show: true,     //是否显示
+            //     ok: false,      //是否已完成
+            //     edit: false,    //当前是否可编辑
+            //     text: "123456", //内容
+            // },
         ],
+        todoscomplete: 0,      //已完成数量
+        activate: 'all'        //当前选择的页面全部选项all   已完成的completed     未完成的unfinished
     },
     // 计算属性
     computed: {
+        // 剩余未完成的项目
         count: function () {
-            return this.todos.length;
+            return this.todos.length - this.todoscomplete;
         }
-    },
-    // 过滤器
-    filters: {
-
     },
     // 方法
     methods: {
+        // 添加一条事项
         add: function (){
-            if(this.message.length === 0) return;
+            if(this.message.length === 0) return;  //如果长度为空则不添加  空白符已用.trim修饰符去除
             this.todos.push({
                 id: this.todos.length,
+                show: true,
                 ok: false,
                 edit: false,
                 text: this.message,
             })
-            // this.message = '';
+            this.message = '';
         },
-        reitem: function(index) {
-            console.log("aa",index);
-            
+        // 删除传入的条目
+        reitem: function(id) {
+            let index = this.todos.indexOf(id);
             this.todos.splice(index,1);
+        },
+        // 全部选中
+        allOk: function() {
+            this.todos.forEach(element => {
+                element.ok = true;
+            });
+            this.todoscomplete = this.todos.length;
+        },
+        // 更新已完成的数量  n子组件通过自定义事件传入
+        updatasize: function(n) {
+            this.todoscomplete += n;
+        },
+        //清除已完成的选项
+        clearOk: function() {
+            this.todos = this.todos.filter(element=>{
+                return !element.ok;
+            })
+            this.todoscomplete = 0;
+        },
+        // 切换筛选页面
+        // 全部页面，所有选项的show属性设为true
+        allPage: function() {
+            this.activate = 'all';
+            this.todos.map(element=>element.show = true);
+        },
+        // 未完成页面，只有ok属性为false的选项的show属性设为true
+        unfinishedPage: function() {
+            this.activate = 'unfinished';
+            this.todos.map(element=>element.show = !element.ok);
+        },
+        // 已完成的页面，ok属性为true的选项的show属性设为true
+        completedPage: function() {
+            this.activate = 'completed';
+            this.todos.map(element=>element.show = element.ok);
         }
-    },
-    // 监听器
-    watch: {
-
     },
     // 注册局部组件
     components: {
