fbd04a95cfe9c690dbd64bc7fa98a5515d0a449b
unknown <email@xluos.com>
2018-06-04 22:31:49 +0800

    增加设计图

--- Async/Async-Await.js
+++ Async/Async-Await.js
@@ -1,29 +1,30 @@
 async function _1() {
     setTimeout(() => {
-        console.log(1,"set");
-    }, 300);
+        console.log(1, Date.now());
+    }, 1000);
     return 1;
 }
 
 async function _2() {
     setTimeout(() => {
-        console.log(2,"set");
-    }, 200);
+        console.log(2, Date.now());
+    }, 1000);
     return 2;
 }
 
 async function _3() {
     setTimeout(() => {
-        console.log(3,"set");
-    }, 10);
+        console.log(3, Date.now());
+    }, 1000);
     return 3;
 }
 
-async function main() {
-    console.log(_1());
-    console.log(_2());
-    console.log(_3());
-}
+// async function main() {
+//     console.log(await _1(), Date.now());
+//     console.log(await _2(), Date.now());
+//     console.log(await _3(), Date.now());
+// }
+
 
 // const delay = timeout => new Promise(resolve=> {setTimeout(resolve, timeout); console.log(timeout);
 // });
@@ -31,7 +32,7 @@ async function main() {
 async function delay(timeout) {
     return new Promise(function(resolve){
         setTimeout(resolve, timeout);
-        console.log(timeout);
+        console.log(timeout,Date.now());
     })
 }
 
@@ -41,9 +42,5 @@ async function f(){
     await delay(1000);
     return 'done';
 }
-(async function(){
-     f();
-     f();
-})();
-// f().then(v => console.log(v)); // 等待6s后才输出 'done'
-// f().then(v => console.log(v))
+f().then(v => console.log(v)); // 等待3s后才输出 'done'
+f().then(v => console.log(v));
